bf7208ffe1a6437114bafe58984bd71e79a3bf29
Grzegorz Swiecinski <grzegorz.swiecinski@gmail.com>
2021-02-01 11:59:35 +0100

    Start apki z bootstrapem

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def home
+  end
+  def index
+
+  end
+
+  def cennik
+
+  end
+
+  def kontakt
+
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get 'realizacje', to: 'pages#index'
+  get 'cennik', to: 'pages#cennik'
+  get 'kontakt', to: 'pages#kontakt'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
