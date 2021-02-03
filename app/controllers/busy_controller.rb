876c6ea61c484750a0817f09011206479cd05ecf
Grzegorz Swiecinski <grzegorz.swiecinski@gmail.com>
2021-02-03 23:26:38 +0100

    Kompletny CRUD dla Busy

--- app/controllers/busy_controller.rb
+++ app/controllers/busy_controller.rb
@@ -1,4 +1,9 @@
 class BusyController < ApplicationController
+    before_action :set_busy, only: [:show, :edit, :update, :destroy]
+    
+    def show
+
+    end
 
     def index
         @busy = Busy.all
@@ -11,16 +16,38 @@ class BusyController < ApplicationController
     def create
         @busy = Busy.new(busy_params)
         if @busy.save
-        
+            flash[:notice] = "Dodano realizację"
             redirect_to busy_index_path
         else
             render 'new'
         end
     end
 
+    def edit
+
+    end
+    def update
+    
+        if @busy.update(busy_params)
+            flash[:notice] = "Poprawnie edytowano realizację."
+            redirect_to @busy
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @busy.destroy
+        redirect_to busy_index_path
+    end
+
     private
     def busy_params
         params.require(:busy).permit(:name,:opis)
+    end
+
+    def set_busy
+        @busy = Busy.find(params[:id])
     end
 end
   
--- app/models/busy.rb
+++ app/models/busy.rb
@@ -1,2 +1,4 @@
 class Busy < ApplicationRecord
+    validates :name, presence: true, length: {minimum:1, maximum:100}
+    validates :opis, presence: true, length: {minimum:1, maximum:1000}
 end
